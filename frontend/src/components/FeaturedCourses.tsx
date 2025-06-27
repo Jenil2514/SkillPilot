abd7f2d1cfa7c436f846da28926ff8821995f1ef
jenil2514 <jenilgoswami938@gmail.com>
2025-06-27 17:21:27 +0530

    coursepage bug

--- frontend/src/components/FeaturedCourses.tsx
+++ frontend/src/components/FeaturedCourses.tsx
@@ -170,7 +170,7 @@ const FeaturedCourses = () => {
                         className="bg-purple-600 text-white px-4 py-2 rounded-md hover:bg-purple-700 transition-colors text-sm font-medium"
                         
                       >
-                       <Link to={`/course/${course._id}`} >View Course 
+                       <Link to={`/courses/${course._id}`} >View Course 
                         </Link>
                       </button>
                     </div>
